ea362ac6f8655ffa4ad2e8072f45f26a551f6755
jessicanwey <jessica.n.wey@gmail.com>
2023-12-20 17:52:44 -0500

    Mostly working, but notes list not updating in real time

--- Develop/server.js
+++ Develop/server.js
@@ -1,44 +1,60 @@
- const fs = require('fs');
- const express = require('express');
- const path = require('path');
- const noteData = require('./db/db.json');
- const PORT = process.env.port || 3001;
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+const noteData = require("./db/db.json");
+const PORT = 3001;
+const cuid = require("cuid");
 
- const app = express();
+const app = express();
 
- app.use(express.json());
- app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
- app.use(express.static('public'));
+app.use(express.static("public"));
 
- app.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, 'index.html'));
- });
-
- app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
- });
-
- app.get('/api/notes', (req, res) => res.json(noteData));
-
- app.post('/api/notes', (req, res) => {
- res.json(`${req.method}`);
- console.log(`${req.method}`);
- const title = req.params.title;
- const text = req.params.text;
- const body = JSON.stringify(req.body);
- console.log(title + " " + text + " " + body);
-
- const note = req.body;
-
- noteData.push(note);
-
- fs.writeFileSync('./db/db.json', JSON.stringify(note));
-
- res.json(noteData);
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
- app.listen(PORT, () =>
-    console.log(`Note taker app listening at http://localhost:${PORT}`)
- );
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => res.json(noteData));
+
+app.post("/api/notes", (req, res) => {
+  const body = JSON.stringify(req.body);
+  console.log(body);
+
+  const { title, text } = req.body;
+  const newNote = {
+    title,
+    text,
+    id: cuid(),
+  };
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const notesList = JSON.parse(data);
+
+        notesList.push(newNote);
+
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(notesList),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info("New note has been added successfully.")
+        );
+        console.log(notesList);
+      }
+    });
+    res.json(newNote);
+});
+
+app.listen(PORT, () =>
+  console.log(`Note taker app listening at http://localhost:${PORT}`)
+);
