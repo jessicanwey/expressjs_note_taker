c03ba7133a798e4728f6ec537bcbef1d431bb3fb
jessicanwey <jessica.n.wey@gmail.com>
2023-12-21 14:57:51 -0500

    Added cooments and updated data in json file

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
+// Import all packages needed
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-const noteData = require("./db/db.json");
-const PORT = 3001;
 const cuid = require("cuid");
 
+// Require JSON file and set to variable noteData
+const noteData = require("./db/db.json");
+const PORT = 3001;
+
+// Initialize app variable
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Add a static middleware for serving assets in the public folder
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -26,6 +31,7 @@ app.post("/api/notes", (req, res) => {
   const body = JSON.stringify(req.body);
   console.log(body);
 
+  // Get the new note and add a unique id using CUID
   const { title, text } = req.body;
   const newNote = {
     title,
